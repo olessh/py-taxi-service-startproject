626148faaf03e736cdc48d1b2fe230cadc3ab369
Oles Shulzhenko <simplemanua@gmail.com>
2024-06-15 12:08:40 +0300

    add str method car and driver

--- taxi/models.py
+++ taxi/models.py
@@ -19,8 +19,14 @@ class Driver(AbstractUser):
         verbose_name = "driver"
         verbose_name_plural = "drivers"
 
+    def __str__(self):
+        return f"{self.username} ({self.license_number})"
+
 
 class Car(models.Model):
     model = models.CharField(max_length=255)
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
     drivers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="cars")
+
+    def __str__(self):
+        return f"{self.manufacturer.name} {self.model}"
